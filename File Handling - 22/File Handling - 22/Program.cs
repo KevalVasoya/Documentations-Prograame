8cde6e91c926ede1a5ee4cef8287dc92ed5443de
KevalVasoya <133115041+KevalVasoya@users.noreply.github.com>
2023-06-15 10:13:01 +0530

    Add files via upload

---
+++ File Handling - 22/File Handling - 22/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ReadWriteFile
+{
+
+    public class handle
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            string data;
+            StreamReader reader = null;
+            try
+            {
+                 reader = new StreamReader("C:\\myTestFile.txt");
+                data = reader.ReadLine();   
+
+                while (data != null)
+                {
+                    Console.WriteLine(data);
+                    data = reader.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally 
+            {
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ Upload - 23/Upload - 23/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+class ImageUploader
+{
+    /// <summary>Defines the entry point of the application.</summary>
+    /// <exception cref="System.IO.FileNotFoundException">The specified file was not found.</exception>
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Please provide the path to the image file:");
+            string filePath = Console.ReadLine();
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("The specified file was not found.");
+            }
+
+            Console.WriteLine("Please provide the destination folder path:");
+            string destinationFolderPath = Console.ReadLine();
+
+
+            // give a path 
+            string fileName = Path.GetFileName(filePath);
+
+            // Give a path to image save
+            string destinationPath = Path.Combine(destinationFolderPath, fileName);
+
+            // Copy the file to the destination path
+            File.Copy(filePath, destinationPath, true);
+
+
+
+            Console.WriteLine("Image uploaded successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while uploading the image:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
